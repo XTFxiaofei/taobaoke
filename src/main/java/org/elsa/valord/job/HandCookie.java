844c5268566c78a042b7a8d5602896f3e9ebdc9c
valor <valord577@gmail.com>
2018-09-19 18:06:49 +0800

    app 1.1 尝试修复cookie过期问题

--- src/main/java/org/elsa/valord/job/HandCookie.java
+++ src/main/java/org/elsa/valord/job/HandCookie.java
@@ -23,39 +23,46 @@ public class HandCookie {
 
     private static StringBuilder cookie = new StringBuilder();
 
-    private static final String MY_UNION = "https://pub.alimama.com/myunion.htm";
+    private static final String MY_UNION = "https://pub.alimama.com/myunion.htm?spm=a219t.7900221/1.1998910419.db9f5f632.2a8f75a5EBZv2M#!/report/detail/taoke";
 
     private SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 
-    @Scheduled(cron = "0 0/2 * * * ?")
-    public void hand1() {
-        if (null != headers.getTbCookie()) {
-            AuctionCode.Codes codes = Alimama.getAuctionCode("562382995602");
-            if (null != codes) {
-                log.info("====== Scheduled job execute successfully. ======");
-            }
-        }
-    }
+//    @Scheduled(cron = "0 0/10 * * * ?")
+//    public void hand1() {
+//        if (null != headers.getTbCookie()) {
+//            AuctionCode.Codes codes = Alimama.getAuctionCode("562382995602");
+//            if (null != codes) {
+//                log.info("====== Scheduled job execute successfully. ======");
+//            }
+//        }
+//    }
 
-    @Scheduled(cron = "33 0/4 * * * ?")
+    @Scheduled(cron = "0 0/15 * * * ?")
     public void hand2() {
         if (null != headers.getTbCookie()) {
             Chromes.openTabAndFocus(MY_UNION);
-            if (Chromes.scanUrl(new String[]{MY_UNION})) {
 
-                log.info("StringBuilder => " + cookie);
-                if (cookie.length() > 0) {
-                    cookie.delete(0, cookie.length());
-                }
-                for (Cookie elem : Chromes.getCookies()) {
-                    cookie.append(elem.getName()).append("=").append(elem.getValue()).append(";");
-                }
+            try {
+                Thread.sleep(1000L);
 
-                boolean f = headers.putCookie(Headers.TB, cookie.toString());
-                if (f) {
-                    log.info("taobao cookie: " + headers.getTbCookie());
-                    log.info("====== Scheduled change Cookies successfully. ======");
+                if (Chromes.scanUrl(new String[]{MY_UNION})) {
+
+                    log.info("StringBuilder => " + cookie);
+                    if (cookie.length() > 0) {
+                        cookie.delete(0, cookie.length());
+                    }
+                    for (Cookie elem : Chromes.getCookies()) {
+                        cookie.append(elem.getName()).append("=").append(elem.getValue()).append(";");
+                    }
+
+                    boolean f = headers.putCookie(Headers.TB, cookie.toString());
+                    if (f) {
+                        log.info("taobao cookie: " + headers.getTbCookie());
+                        log.info("====== Scheduled change Cookies successfully. ======");
+                    }
                 }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
 
             Chromes.close();
--- src/main/java/org/elsa/valord/service/link/LinkService.java
+++
@@ -1,8 +0,0 @@
-package org.elsa.valord.service.link;
-
-/**
- * @author valor
- * @date 2018/9/6 13:08
- */
-public interface LinkService {
-}
